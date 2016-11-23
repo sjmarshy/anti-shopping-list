5fb6cb00a9e9c662973d9bb7af781fab8bd9778c
Sam Marshall <sam@Sams-MacBook-Pro.local>
2016-11-23 20:33:12 +0000

    bind localStorage to the correct context

--- src/effects/storage.js
+++ src/effects/storage.js
@@ -3,7 +3,7 @@ import localStorage, { getItemAndParse } from '../util/localStorage';
 import { effect } from '../util/effect';
 
 export function set(key, val) {
-    return effect(localStorage.setItem, [key, JSON.stringify(val)]);
+    return effect(localStorage.setItem.bind(localStorage), [key, JSON.stringify(val)]);
 }
 
 export function get(key) {
