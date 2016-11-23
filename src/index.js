beb2068701d86038d3bb8da643254c630af7f06c
Sam Marshall <sam@Sams-MacBook-Pro.local>
2016-11-23 20:35:50 +0000

    testing index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import 'babel-polyfill'; // eslint-disable-line
+import store from './store';
+import { actions, actionCreators } from './state/shoppinglist';
+
+store.subscribe(() => console.log(store.getState())); // eslint-disable-line
+store.subscribe(() => console.log(localStorage)); // eslint-disable-line
+
+store.dispatch(actionCreators[actions.LOAD]());
+setTimeout(() => {
+    store.dispatch(actionCreators[actions.ADD]({
+        link: 'hih',
+    }));
+}, 2000);
