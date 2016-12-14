c00d1c98e08af70112124f83aba0afd047ae68de
Sam Marshall <sjmarshy@gmail.com>
2016-12-14 19:20:48 +0000

    fix tests

--- src/state/shoppinglist.test.js
+++ src/state/shoppinglist.test.js
@@ -9,62 +9,78 @@ const price = 8.34;
 const displayedLast = Date.now();
 
 test('shoppinglist actionCreators', (t) => {
-    const expectedAddAction = {
-        type: actions.ADD,
-        payload: {
-            link,
-            name,
-            description,
-            price,
-            displayedLast,
-        },
-    };
-    const actualAddAction = actionCreators[actions.ADD](
-        { name, description, link, price, displayedLast });
+    t.test('add', (tt) => {
+        const expectedAddAction = {
+            type: actions.ADD,
+            payload: {
+                link,
+                name,
+                description,
+                price,
+                displayedLast,
+            },
+        };
+        const actualAddAction = actionCreators[actions.ADD](
+            { name, description, link, price, displayedLast });
 
-    t.deepEqual(actualAddAction, expectedAddAction, 'add action creator');
+        tt.deepEqual(actualAddAction, expectedAddAction, 'add action creator');
+        tt.end();
+    });
 
-    const expectedRemoveAction = {
-        type: actions.REMOVE,
-        payload: link,
-    };
-    const actualRemoveAction = actionCreators[actions.REMOVE](link);
+    t.test('remove', (tt) => {
+        const expectedRemoveAction = {
+            type: actions.REMOVE,
+            payload: link,
+        };
+        const actualRemoveAction = actionCreators[actions.REMOVE](link);
 
-    t.deepEqual(actualRemoveAction, expectedRemoveAction, 'remove action creator');
+        tt.deepEqual(actualRemoveAction, expectedRemoveAction, 'remove action creator');
+
+        tt.end();
+    });
 
     t.end();
 });
 
 test('shoppinglist actionHandlers', (t) => {
-    const action = actionCreators[actions.ADD](
-        { name, description, link, price, displayedLast });
-    const expected = [action.payload];
-    const actual = actionHandlers([])[action.type](action.payload);
+    t.test('add', (tt) => {
+        const action = actionCreators[actions.ADD](
+            { name, description, link, price, displayedLast });
+        const expected = [action.payload];
+        const actual = actionHandlers([])[action.type](action.payload);
 
-    t.deepEqual(actual, expected, 'should add a new shoppinglist item to state');
+        tt.deepEqual(actual, expected, 'should add a new shoppinglist item to state');
 
-    const doubleActual = actionHandlers(
-        actionHandlers([])[action.type](action.payload),
-    )[action.type](action.payload);
+        const doubleActual = actionHandlers(
+                actionHandlers([])[action.type](action.payload),
+            )[action.type](action.payload);
 
-    t.deepEqual(doubleActual, expected, 'should not add the same link twice');
+        tt.deepEqual(doubleActual, expected, 'should not add the same link twice');
 
-    const state = [];
+        const state = [];
 
-    actionHandlers(state)[action.type](action.payload);
+        actionHandlers(state)[action.type](action.payload);
 
-    t.equal(0, state.length, 'addition should not effect the original state');
+        tt.equal(0, state.length, 'addition should not effect the original state');
+        tt.end();
+    });
 
-    const removeAction = actionCreators[actions.REMOVE](link);
-    const removeExpected = [];
-    const removeActual = actionHandlers([action.payload])[removeAction.type](removeAction.payload);
+    t.test('remove', (tt) => {
+        const removeCreator = actionCreators[actions.REMOVE];
+        const startingState = [{ link }];
+        const removeHandler = actionHandlers(startingState)[actions.REMOVE];
 
-    t.deepEqual(removeActual, removeExpected, 'should remove a shoppinglist item from state based on the link');
+        const action = removeCreator(link);
+        const expected = [];
+        const actual = removeHandler(link);
 
-    const state2 = [action.payload];
+        tt.deepEqual(actual, expected, 'should remove a shoppinglist item from state based on the link');
 
-    actionHandlers(state2)[removeAction.type](removeAction.payload);
+        const state2 = [action.payload];
 
-    t.equal(1, state2.length, 'remove should not affect the original state');
-    t.end();
+        actionHandlers(state2)[action.type](action.payload);
+
+        tt.equal(1, state2.length, 'remove should not affect the original state');
+        tt.end();
+    });
 });
