bd36a80247380e84058103850a9aeb89c2f0bcf5
Sam Marshall <sjmarshy@gmail.com>
2017-02-26 15:27:47 +0000

    cleanup

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,21 +1,31 @@
 import { h } from 'preact'; // eslint-disable-line
 
-export default () => <div className="main-AddItem">
-  <form className="main-AddItem_Form">
-    <input
-      className="main-AddItem_NameInput"
-      type="text"
-      placeholder="name"
-    />
-    <input
-      className="main-AddItem_LinkInput"
-      type="text"
-      placeholder="link"
-    />
-    <input
-      className="main-AddItem_AddButton"
-      type="button"
-      value="Add Product"
-    />
-  </form>
-</div>;
+const AddItem = ({
+    onNameChange,
+    onLinkChange,
+    onSubmit,
+}) => (
+    <div className="main-AddItem">
+        <form className="main-AddItem_Form" onSubmit={onSubmit}>
+            <input
+                className="main-AddItem_NameInput"
+                type="text"
+                placeholder="name"
+                onChange={onNameChange}
+            />
+            <input
+                className="main-AddItem_LinkInput"
+                type="text"
+                placeholder="link"
+                onChange={onLinkChange}
+            />
+            <input
+                className="main-AddItem_AddButton"
+                type="button"
+                value="Add Product"
+            />
+        </form>
+    </div>
+);
+
+export default AddItem;
